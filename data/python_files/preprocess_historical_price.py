47a378de5ac04c8c0233bacdfc31d9e9264393d3
TianleiHe <tianleihe@g.harvard.edu>
2021-04-18 13:30:48 -0400

    update preprocess step and return 1*20 result

--- data/python_files/preprocess_historical_price.py
+++ data/python_files/preprocess_historical_price.py
@@ -1,10 +1,11 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as f
+import numpy as np
 
 spark = SparkSession.builder.master('local').appName('NewsSentimentAnalysis').getOrCreate()
 
 # read csv
-df = spark.read.csv('../data/ticker_RIOT.csv', header=True)
+df = spark.read.csv('alldate_RIOT.csv', header=True)
 
 df = df.withColumn('Date', f.to_timestamp(df['Date'], 'yyyy-MM-dd'))
 
@@ -13,15 +14,19 @@ df = df.withColumn('Date', f.to_timestamp(df['Date'], 'yyyy-MM-dd'))
 def combine(opening_price, closing_price):
     return [opening_price, closing_price]
 
-
 df = df.withColumn('Open_Close', combine(df['Open'], df['Close']))
-# print(df.show())
+df = df.withColumn("Open_Close_new", f.split(f.regexp_replace("Open_Close", r"(^\[)|(\]$)", ""), ", ").cast("array<float>"))
+# df.show()
+# df.printSchema()
+
+# print([row['Open_Close_new'] for row in df.collect()])
 
 window_duration = '10 day'
 slide_duration = '1 day'
 
 df = df.groupBy(f.window('Date', window_duration, slide_duration)) \
-    .agg(f.collect_list('Open')) \
-    .withColumnRenamed('collect_list(Open)', 'sliding_window')
+    .agg(f.collect_list('Open_Close_new')) \
+    .withColumnRenamed('collect_list(Open_Close_new)', 'sliding_window')
 
-print(df.show())
+print([(row['window'],np.array(row['sliding_window']).reshape([-1]).tolist()) for row in df.collect()])
+
