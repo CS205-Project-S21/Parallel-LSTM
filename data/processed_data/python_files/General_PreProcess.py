42d511ad080d04c74cfa9395bdf54ecf2c7224f4
TianleiHe <tianleihe@g.harvard.edu>
2021-05-05 20:24:46 -0400

    upload long time general preprocess

--- data/processed_data/python_files/General_PreProcess.py
+++ data/processed_data/python_files/General_PreProcess.py
@@ -12,23 +12,15 @@ import pandas as pd
 import sys
 
 
-def main():
-    stockprice_rawdata_path = '../../stock_price_Energy/data/price_COG.csv'
-    news_rawdata_path = '../../news_Energy/data/GoogleNews_Energy_large_all.csv'
+##########################################  1. For StockPrice Data ###################################################
 
-    spark = SparkSession.builder.master('local[2]').appName('GeneralDataProcess').getOrCreate()
-
+def preprocess_price(price_name, price_path, spark, startdate, enddate):
     window_duration = '11 day'
     slide_duration = '1 day'
     num_datapoints = 22
 
-    startdate = datetime.date(2016, 3, 31)
-    enddate = datetime.date(2021, 4, 16)
-
-    ##########################################  1. For StockPrice Data ###################################################
-    ## read csv
-    df = spark.read.csv(stockprice_rawdata_path, header=True)
-
+    # read csv
+    df = spark.read.csv(price_path, header=True)
     # copy date to create a new time check colume
     df = df.withColumn('Date', f.to_timestamp(df['Date'], 'yyyy-MM-dd'))
     df2 = df.withColumn('TrueDate', f.to_timestamp(df['Date'], 'yyyy-MM-dd')) \
@@ -91,7 +83,11 @@ def main():
     df_filled_temp = df_filled.withColumn('if_open', f.when(f.col('Type') == 'Open', 1).otherwise(0))
     df_filled2 = df_filled_temp.withColumn('Price_interpol',
                                            f.when(f.col('readtime_bf') == f.col('readtime_ff'), f.col('Price')) \
-                                           .otherwise((f.col('readvalue_bf') - f.col('readvalue_ff')) / (f.col('readtime_bf').cast("long") - f.col('readtime_ff').cast("long") - 43200) * (f.col('TrueDate').cast("long") - f.col('readtime_ff').cast("long") - 43200 * f.col('if_open')) + f.col('readvalue_ff')))
+                                           .otherwise((f.col('readvalue_bf') - f.col('readvalue_ff')) / (
+                                                       f.col('readtime_bf').cast("long") - f.col('readtime_ff').cast(
+                                                   "long") - 43200) * (f.col('TrueDate').cast("long") - f.col(
+                                               'readtime_ff').cast("long") - 43200 * f.col('if_open')) + f.col(
+                                               'readvalue_ff')))
     df4 = df_filled2.select('TrueDate', 'Type', 'Price_interpol')
 
     # df4 looks like:
@@ -126,10 +122,12 @@ def main():
         x_normalized = ((x - np.min(x)) / (np.max(x) - np.min(x))).tolist()
         return x_normalized
 
-    # Kick out rows with less than num_datapoints data points
+        # Kick out rows with less than num_datapoints data points
+
     df6 = df6.withColumn('array_length', f.size("StockPrice"))
     df6 = df6.filter((df6.array_length == num_datapoints)).select(['window', 'StockPrice'])
     df6 = df6.withColumn('StockPrice', normalize(df6['StockPrice']))
+    df_price = df6.withColumnRenamed('StockPrice', 'StockPrice_{}'.format(price_name))
     # df6 looks like:
     # +--------------------+--------------------+
     # |              window|          StockPrice|
@@ -140,24 +138,31 @@ def main():
     # |{2016-04-02 20:00...|[0.0, 0.0, 0.0, 0...|
     # |{2016-04-03 20:00...|[0.0, 0.0, 0.5434...|
     # |{2016-04-04 20:00...|[0.0, 0.499999574...|
+    return df_price
 
-    ##########################################  2. For sentiment analysis ###################################################
-    df7 = spark.read.csv(news_rawdata_path, header=True, escape='"')
-    df7 = df7.withColumn('time', f.to_timestamp(df7['datetime'], 'yyyy-MM-dd HH:mm:ss'))
-    df7 = df7.withColumn('hour', f.hour(f.col('time')))
-    df7 = df7.withColumn('Day', f.to_date(df7['time'], format='yyyy-MM-dd'))
+
+##########################################  2. For sentiment analysis ###################################################
+def preprocess_news(news_path, spark, startdate, enddate):
+    window_duration = '11 day'
+    slide_duration = '1 day'
+    num_datapoints = 22
+
+    df = spark.read.csv(news_path, header=True, escape='"')
+    df = df.withColumn('time', f.to_timestamp(df['datetime'], 'yyyy-MM-dd HH:mm:ss'))
+    df = df.withColumn('hour', f.hour(f.col('time')))
+    df = df.withColumn('Day', f.to_date(df['time'], format='yyyy-MM-dd'))
 
     @f.udf
     def connect_string(a, b):
         return a + ' ' + b
 
-    df7 = df7.withColumn('AllText', connect_string(df7['title'], df7['desc']))
-    df8 = df7.select('Day', 'hour', 'Alltext')
-    df8 = df8.withColumn('TrueDate', when(df8.hour < 9, df8['Day'])
-                         .when(df8.hour >= 16, f.date_add(df8['Day'], 1))
-                         .otherwise(df8['Day']))
-    df8 = df8.withColumn('Type', when(df8.hour < 9, 'Open')
-                         .when(df8.hour >= 16, 'Open')
+    df = df.withColumn('AllText', connect_string(df['title'], df['desc']))
+    df1 = df.select('Day', 'hour', 'Alltext')
+    df1 = df1.withColumn('TrueDate', when(df1.hour < 9, df1['Day'])
+                         .when(df1.hour >= 16, f.date_add(df1['Day'], 1))
+                         .otherwise(df1['Day']))
+    df1 = df1.withColumn('Type', when(df1.hour < 9, 'Open')
+                         .when(df1.hour >= 16, 'Open')
                          .otherwise('Close'))
 
     # calculate sentiment scores for title, description and content
@@ -168,8 +173,8 @@ def main():
         score = analyzer.polarity_scores(text)['compound']
         return score
 
-    df9 = df8.withColumn('score', calculate_sentiment_score(df8['Alltext']))
-    df9 = df9.groupBy(['TrueDate', 'Type']).agg(f.avg("score").alias("AverageScore"))
+    df2 = df1.withColumn('score', calculate_sentiment_score(df1['Alltext']))
+    df2 = df2.groupBy(['TrueDate', 'Type']).agg(f.avg("score").alias("AverageScore"))
 
     # Fill the missing row with help of a full dateframe
     full_dict = {'TrueDate': [], 'Type': []}
@@ -180,64 +185,48 @@ def main():
         cdate += datetime.timedelta(days=1)
     df_ref_pd = pd.DataFrame(full_dict)
     df_ref = spark.createDataFrame(df_ref_pd)
-    df10 = df_ref.join(df9, on=['TrueDate', 'Type'], how='left_outer')
-    df10 = df10.na.fill(value=0, subset=["AverageScore"])
+    df3 = df_ref.join(df2, on=['TrueDate', 'Type'], how='left_outer')
+    df3 = df3.na.fill(value=0, subset=["AverageScore"])
 
     # use the following to combine open and close into an array
     w = Window.partitionBy('TrueDate').orderBy(f.desc('Type'))
-    df11 = df10.withColumn(
+    df4 = df3.withColumn(
         'Open_Close', f.collect_list('AverageScore').over(w)
     ) \
         .groupBy('TrueDate').agg(f.max('Open_Close').alias('Open_Close'))
 
     # Create a time window, and collect to form a larger array
-    df12 = df11.orderBy("TrueDate").groupBy(f.window('TrueDate', window_duration, slide_duration)) \
+    df5 = df4.orderBy("TrueDate").groupBy(f.window('TrueDate', window_duration, slide_duration)) \
         .agg(f.collect_list('Open_Close')) \
         .withColumnRenamed('collect_list(Open_Close)', 'NewsScore')
-    df12 = df12.withColumn('NewsScore', f.flatten(df12['NewsScore'])).sort("window")
+    df5 = df5.withColumn('NewsScore', f.flatten(df5['NewsScore'])).sort("window")
 
     # Kick out rows with less than num_datapoints data points
-    df12 = df12.withColumn('array_length', f.size("NewsScore"))
-    df12 = df12.filter((df12.array_length == num_datapoints)).select(['window', 'NewsScore'])
+    df5 = df5.withColumn('array_length', f.size("NewsScore"))
+    df5 = df5.filter((df5.array_length == num_datapoints)).select(['window', 'NewsScore'])
+    return df5
 
-    ##########################################  3. Combine df6 and df12 ################################################
-    df_final = df6.join(df12, on=['window'], how='left_outer')
 
-    # df_final.orderBy('window') looks like:
-    # +--------------------+--------------------+--------------------+
-    # |              window|          StockPrice|           NewsScore|
-    # +--------------------+--------------------+--------------------+
-    # |{2016-03-30 20:00...|[0.0, 0.500000152...|[0.0, 0.0, 0.0, 0...|
-    # |{2016-03-31 20:00...|[0.0, 0.061224916...|[0.0, 0.0, -0.130...|
-    # |{2016-04-01 20:00...|[0.0, 0.0, 0.0, 0...|[-0.1307, 0.0, 0....|
-    # |{2016-04-02 20:00...|[0.0, 0.0, 0.0, 0...|[0.0, 0.0, 0.9878...|
-    # |{2016-04-03 20:00...|[0.0, 0.0, 0.5434...|[0.9878, 0.0, -0....|
-    # |{2016-04-04 20:00...|[0.0, 0.499999574...|[-0.1027, 0.0, 0....|
-    # |{2016-04-05 20:00...|[0.63157815484152...|[0.0, 0.0, 0.1104...|
-    # |{2016-04-06 20:00...|[0.05263151290346...|[0.1104, 0.0, 0.0...|
-    # |{2016-04-07 20:00...|[0.21052605161384...|[0.0, 0.0, 0.0, 0...|
-    df_final.orderBy('window').toPandas().to_csv('processed_data_energy_COG.csv', index=False)
+##########################################  4. Combine all price and sentiment analysis ###################################################
+def main():
+    stockprice_rawdata_path_COG = '../../stock_price_Energy/data/price_COG.csv'
+    stockprice_rawdata_path_DVN = '../../stock_price_Energy/data/price_DVN.csv'
+    stockprice_rawdata_path_HFC = '../../stock_price_Energy/data/price_HFC.csv'
+    news_rawdata_path = '../../news_Energy/data/GoogleNews_Energy_large_all.csv'
+    spark = SparkSession.builder.master('local[2]').appName('GeneralDataProcess').getOrCreate()
+
+    sdate = datetime.date(2016, 3, 31)
+    edate = datetime.date(2021, 4, 16)
+
+    df_COG = preprocess_price('COG', stockprice_rawdata_path_COG, spark, sdate, edate)
+    df_DVN = preprocess_price('DVN', stockprice_rawdata_path_DVN, spark, sdate, edate)
+    df_HFC = preprocess_price('HFC', stockprice_rawdata_path_HFC, spark, sdate, edate)
+    df_news = preprocess_news(news_rawdata_path, spark, sdate, edate)
+
+    df_price = df_COG.join((df_DVN.join(df_HFC, on=['window'], how='left_outer')), on=['window'], how='left_outer')
+    df_final = df_news.join(df_price, on=['window'], how='left_outer')
+
+    df_final.orderBy('window').toPandas().to_csv('processed_data_energy.csv', index=False)
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
